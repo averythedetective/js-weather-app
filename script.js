425bdc3c8a52982590a935d3e02d3a92e9206e69
Avery Izanov <vigilante_86@protonmail.com>
2025-03-24 20:05:42 +0500

    Style and fetch method changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getWeather() {
+async function getWeather() {
   const apiKey = '9a4c596ddd1a96f185cc513d0538ac57';
   const city = document.getElementById('city').value;
 
@@ -9,26 +9,28 @@ function getWeather() {
 
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+  
+  try {
+    let response = await fetch(currentWeatherUrl);
+    const data = await response.json();
+    displayWeather(data);
+    console.log(data);
 
-  fetch(currentWeatherUrl)
-    .then((response) => response.json())
-    .then((data) => { 
-      displayWeather(data)
-    })
-    .catch((error) => {
-      console.error('Error fetching current weather data:', error);
-      alert('Error fetching current weather data. Please try again.');
-    });
+  } catch(error) {
+    console.warn('Error fetching weather data:', error);
+    alert('Error fetching weather data. Please try again.');
+  }
 
-    fetch(forecastUrl)
-    .then((response) => response.json())
-    .then((data) => { 
-      displayHourlyForecast(data.list);
-    })
-    .catch((error) => {
-      console.error('Error fetching hourly forecast data:', error);
-      alert('Error fetching hourly forecast data. Please try again.');
-    });
+  try {
+    let response = await fetch(forecastUrl);
+    const data = await response.json();
+    displayHourlyForecast(data.list);
+    console.log(data.list);
+
+  } catch(error) {
+    console.warn('Error fetching hourly forecast data:', error);
+    alert('Error fetching hourly forecast data. Please try again.');
+  }
 }
 
 function displayWeather(data) {
@@ -75,13 +77,13 @@ function displayHourlyForecast(hourlyData) {
   const next24Hours = hourlyData.slice(0, 8); // Display the next 24 hours (3-hour intervals)
 
   next24Hours.forEach(item => {
-      const dateTime = new Date(item.dt * 1000); // Convert timestamp to milliseconds
-      const hour = dateTime.getHours();
-      const temperature = Math.round(item.main.temp - 273.15); // Convert to Celsius
-      const iconCode = item.weather[0].icon;
-      const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+    const dateTime = new Date(item.dt * 1000); // Convert timestamp to milliseconds
+    const hour = dateTime.getHours();
+    const temperature = Math.round(item.main.temp - 273.15); // Convert to Celsius
+    const iconCode = item.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
 
-      const hourlyItemHtml = `
+    const hourlyItemHtml = `
         <div class="hourly-item">
             <span>${hour}:00</span>
             <img src="${iconUrl}" alt="Hourly Weather Icon">
@@ -89,7 +91,7 @@ function displayHourlyForecast(hourlyData) {
         </div>
       `;
 
-      hourlyForecastDiv.innerHTML += hourlyItemHtml;
+    hourlyForecastDiv.innerHTML += hourlyItemHtml;
   });
 }
 
